d8334bb189319c9bf80b4a7ab45449b92bfe98af
Seunghyun Kim <kshlove735@suhwooms.com>
2023-06-23 19:20:15 +0900

    env 제거

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,12 +3,20 @@ import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 import { Response } from 'express';
 import { Public } from 'src/common/decorator/public.decorator';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller()
 export class AuthController {
   constructor(
     private readonly authService: AuthService,
   ) { }
+
+
+  @Public()
+  @Post('auth/signup')
+  async signUp(@Body() createUserDto: CreateUserDto) {
+
+  }
 
   // @HttpCode(HttpStatus.OK)
   @Public()
